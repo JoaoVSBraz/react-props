0890ed13943300764ef8791b8c4f9021814d566f
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-02-22 15:06:17 -0300

    :construction: Add react props at card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,14 @@
 import React from "react"
 
-function Card() {
+function Card(props) {
     return (
         <div>
-            <h2>Beyonce</h2>
+            <h2>{props.name}</h2>
             <img
-                src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-                alt="avatar_img"
+                src={props.imgURL}
+                alt='default'
             />
-            <p>+123 456 789</p>
-            <p>b@beyonce.com</p>
+            <p>{props.description}</p>
         </div>
     )
 }
---
+++ src/contacts.js
@@ -0,0 +1,19 @@
+const contacts = [
+    {
+        name: 'Alan Turing',
+        imgURL: 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Alan_Turing_Aged_16_%28cropped%29.jpg',
+        description: 'Father of Science Computer'
+    },
+    {
+        name: 'Ada Lovelace',
+        imgURL: 'https://codigofonte.com.br/wp-content/uploads/2019/03/Ada_Lovelace_portrait.jpg',
+        description: 'The First Programmer'
+    },
+    {
+        name: 'Claude Shannon',
+        imgURL: 'https://upload.wikimedia.org/wikipedia/commons/9/99/ClaudeShannon_MFO3807.jpg',
+        description: 'Father of Criptography'
+    }
+]
+
+export default contacts
