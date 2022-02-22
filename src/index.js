4a9d1c6b2b1fd66bd7c975853f329fe78afe77c6
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-02-22 15:23:11 -0300

    :art: Create App component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from './components/Card'
-import contacts from './contacts'
+import App from './components/App';
 
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-
-    <Card name={contacts[0].name} imgURL={contacts[0].imgURL} description={contacts[0].description}/>
-    <Card name={contacts[1].name} imgURL={contacts[1].imgURL} description={contacts[1].description}/>
-    <Card name={contacts[2].name} imgURL={contacts[2].imgURL} description={contacts[2].description}/>
-  </div>,
-  document.getElementById("root")
+ReactDOM.render(<App />, document.getElementById("root")
 );
