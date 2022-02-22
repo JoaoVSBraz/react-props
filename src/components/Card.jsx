4fd394c910f90265df927bf161ddf230f00159e2
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-02-22 15:27:26 -0300

    :art: Update card component structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,17 @@ import React from "react"
 function Card(props) {
     return (
         <div>
-            <h2>{props.name}</h2>
-            <img
-                src={props.imgURL}
-                alt='default'
-            />
-            <p>{props.description}</p>
+            <div>
+                <h2>{props.name}</h2>
+                <img
+                    src={props.imgURL}
+                    alt='default'
+                />
+            </div>
+            <div>
+                <p>{props.description}</p>
+            </div>
+            
         </div>
     )
 }
